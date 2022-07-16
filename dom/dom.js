c30d3fe9efe298bb46bdea84b8c083526a1bd735
Illia <chippaxyucac@gmail.com>
2022-07-16 15:47:48 +0200

    chanhge style.css

---
+++ dom/dom.js
@@ -0,0 +1,12 @@
+//alert(Array.from(document.body.childNodes).filter())
+
+const text = document.querySelector('.lesson__text');
+
+const textElementContent = text.innerHTML;
+text.innerHTML = `
+    <h1>What the fuck Jhony?</h1>
+    <p class="yellow-text">We should be go to the cinema!</p>
+`;
+
+
+
